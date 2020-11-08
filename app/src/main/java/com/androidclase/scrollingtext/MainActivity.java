612fed305c7c204b31e4b2f59bb9799ac148bab7
enriquerojasv <erojasv@cifpfbmoll.eu>
2020-11-08 14:26:28 +0100

    added operative button

--- app/src/main/java/com/androidclase/scrollingtext/MainActivity.java
+++ app/src/main/java/com/androidclase/scrollingtext/MainActivity.java
@@ -3,6 +3,8 @@ package com.androidclase.scrollingtext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showComment(View view) {
+        EditText appearComment = findViewById(R.id.comment);
+        appearComment.setVisibility(view.VISIBLE);
     }
 }
